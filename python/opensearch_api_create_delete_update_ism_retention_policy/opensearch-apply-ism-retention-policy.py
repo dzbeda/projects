2cd813621a3bbbee9c6025e96e573ddf950ffca1
dzbeda <44648295+dzbeda@users.noreply.github.com>
2023-10-04 11:35:22 +0300

    Update opensearch-apply-ism-retention-policy.py

--- python/opensearch_api_create_delete_update_ism_retention_policy/opensearch-apply-ism-retention-policy.py
+++ python/opensearch_api_create_delete_update_ism_retention_policy/opensearch-apply-ism-retention-policy.py
@@ -12,8 +12,42 @@ DB_HOSTNAME = 'es1'
 DB_PORT = '9200'
 BASE_URL = '_opendistro/_ism/policies'
 URL_HEADERS = {'content-type': 'application/json'}
-SITE_INFO_FILE_PATH = './input-file.yml'
+SITE_INFO_FILE_PATH = './ism-input.yml'
 
+
+def check_input_file(list_of_ism_retention_policies_to_upload):
+    for policy_name in list_of_ism_retention_policies_to_upload:
+        if policy_name['status'] != 'enable' and policy_name['status'] != 'disable':
+            print('status value is not configured correctly for '+ policy_name['index_name'] + ' index.  status should be set to enable or disable , currently it is set to ' + policy_name['status'] )
+            return False
+def get_policies():
+    active_ism_policies = []
+    active_ism_policies_names = []
+    try:
+        get_response = requests.get(url=f'http://{DB_HOSTNAME}:{DB_PORT}/{BASE_URL}', headers=URL_HEADERS, verify=False)
+    except:
+        print(f'Please check the connection to Elastic server. Elastic Hostname {DB_HOSTNAME} and port {DB_PORT}')
+    get_policies_response = json.loads(get_response.content)
+    for policy in get_policies_response["policies"]:
+        policy_details = {'policy_name': policy['_id'],
+                          'policy_sequential_number': policy['_seq_no'],
+                          'policy_primary_term': policy['_primary_term']}
+        active_ism_policies.append(policy_details)
+    for policy_name_id in active_ism_policies:
+        active_ism_policies_names.append(policy_name_id['policy_name'])
+    return (active_ism_policies,active_ism_policies_names)
+
+def delete_policy(policy_name):
+    delete_response = requests.delete(url=f'http://{DB_HOSTNAME}:{DB_PORT}/{BASE_URL}/{policy_name}', headers=URL_HEADERS,
+                                      verify=False)
+    if delete_response.status_code == 200:
+        logging.info(f'policy {policy_name} was delete successfully')
+        return True
+    else:
+        print('policy {policy_name} was not created - please check connectivity to the server ,check if index prefix'
+              ' already exsists or other policy parameters')
+        print(f'status code is : {delete_response.status_code} and status content is {delete_response.content}')
+        return False
 
 def update_policy_template(policy_name,min_index_age_in_days):
     policy_template = '''{
@@ -61,44 +95,6 @@ def update_policy_template(policy_name,min_index_age_in_days):
     updated_policy = policy_template_obj.substitute(policy_name=policy_name, min_index_age_in_days=min_index_age_in_days)
     return updated_policy
 
-
-def check_input_file(list_of_ism_retention_policies_to_upload):
-    for policy_name in list_of_ism_retention_policies_to_upload:
-        if policy_name['status'] != 'enable' and policy_name['status'] != 'disable':
-            print('status value is not configured correctly for '+ policy_name['index_name'] + ' index.  status should be set to enable or disable , currently it is set to ' + policy_name['status'] )
-            return False
-
-def get_policies():
-    active_ism_policies = []
-    active_ism_policies_names = []
-    try:
-        get_response = requests.get(url=f'http://{DB_HOSTNAME}:{DB_PORT}/{BASE_URL}', headers=URL_HEADERS, verify=False)
-    except:
-        print(f'Please check the connection to Elastic server. Elastic Hostname {DB_HOSTNAME} and port {DB_PORT}')
-    get_policies_response = json.loads(get_response.content)
-    for policy in get_policies_response["policies"]:
-        policy_details = {'policy_name': policy['_id'],
-                          'policy_sequential_number': policy['_seq_no'],
-                          'policy_primary_term': policy['_primary_term']}
-        active_ism_policies.append(policy_details)
-    for policy_name_id in active_ism_policies:
-        active_ism_policies_names.append(policy_name_id['policy_name'])
-    return (active_ism_policies,active_ism_policies_names)
-
-
-def delete_policy(policy_name):
-    delete_response = requests.delete(url=f'http://{DB_HOSTNAME}:{DB_PORT}/{BASE_URL}/{policy_name}', headers=URL_HEADERS,
-                                      verify=False)
-    if delete_response.status_code == 200:
-        logging.info(f'policy {policy_name} was delete successfully')
-        return True
-    else:
-        print('policy {policy_name} was not created - please check connectivity to the server ,check if index prefix'
-              ' already exsists or other policy parameters')
-        print(f'status code is : {delete_response.status_code} and status content is {delete_response.content}')
-        return False
-
-
 def create_policy(policy_name,min_index_age_in_days):
     policy_json_format = update_policy_template(policy_name, min_index_age_in_days)
     create_response = requests.put(url=f'http://{DB_HOSTNAME}:{DB_PORT}/{BASE_URL}/{policy_name}', data=policy_json_format,
@@ -115,7 +111,6 @@ def create_policy(policy_name,min_index_age_in_days):
                       f' prefix already exsists or other policy parameters')
         logging.error(f'status code is : {create_response.status_code} and status content is {create_response.content}')
         return False
-
 
 def update_policy(policy_name,min_index_age_in_days,policy_sequential_number,policy_primary_term):
     policy_json_format = update_policy_template(policy_name, min_index_age_in_days)
@@ -138,7 +133,6 @@ def update_policy(policy_name,min_index_age_in_days,policy_sequential_number,pol
         logging.error(f'status code is : {update_response.status_code} and status content is {update_response.content}')
         return False
 
-
 def main():
     with open(rf'{SITE_INFO_FILE_PATH}') as file:
         opensearch_main_ism_retention_policy = yaml.load(file)
@@ -146,9 +140,7 @@ def main():
         check_input_file_status = check_input_file(list_of_ism_retention_policies_to_upload)
         if check_input_file_status == 'false':
             exit()
-
     active_ism_policies, active_ism_policies_names = get_policies()
-
     for policy in list_of_ism_retention_policies_to_upload:
         if (policy['status']) == 'disable':
             if (policy['index_name']) in active_ism_policies_names:
@@ -177,6 +169,5 @@ def main():
         else:
             logging.error('something is wrong - Please check the "opensearch_main_ism_retention_policy" section under '
                           'site-info.yml file')
-
 if __name__ == '__main__':
     main()
